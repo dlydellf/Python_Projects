d810c2614e1407b8be9a1f19a4eeea1070bad488
DForeman <dlydellf@hotmail.com>
2021-06-26 13:27:51 -0500

    Updated file w/Step 241

--- Inheritance Submission Assignment (Step_237)/Inheritance.py
+++ Inheritance Submission Assignment (Step_237)/Inheritance.py
@@ -3,12 +3,13 @@
 #   Created using: Python/IDLE v3.9.4
 
 # 'Business' is the parent class, created with 5 inheritable attributes:
+'''
 class Business:
-    name = "What is your business's name?"
-    location = "What is your business's address?"
-    phone = "What is your business's phone number?"
-    industry = "What industry does your business operate within?"
-    employees = "How many employees work at your business?"
+    name = input("What is your business's name?")
+    location = input("What is your business's address?")
+    phone = input("What is your business's phone number?")
+    industry = input("What industry does your business operate within?")
+    employees = input("How many employees work at your business?")
 
 
 # 'DayCareCenter' is the 1st child class; it inherits all 5 of the 'Business' \
@@ -25,4 +26,43 @@ class utilityCompany(Business):
     serviceArea = "Southwest"
     EnviroImpactIndex = 23
     lobbyists = True
-        
+'''
+#----------------------------------------------
+#   Objectives of Step 241:
+#       a) Create a class,
+#       b) Create an object,
+#       c) Assign values to the object props using __init__, and...
+#       d) Create a method in the class
+
+class Vehicle: # (Part a)
+    def __init__(self, name, operable, VIN, tires, insured, propulsion): # (Part c)
+        self.name = name
+        self.operable = operable
+        self.VIN = VIN
+        self.tires = tires
+        self.insured = insured
+        self.propulsion = propulsion
+
+    def movement(self): # (Part d)
+        print('Every {} is propelled via {}'.format(self.name, self.propulsion))
+
+bicycle = Vehicle("Schwinn", True, "1000001", True, False, "peddling") # (Part b)
+'''
+print('This vehicle OBJECT is a {}; is it operable, True or False?: {}'.format(bicycle.name, bicycle.operable))
+print("This vehicle's VIN is {}; does it have tires, True or False?: {}".format(bicycle.VIN, bicycle.tires))
+print('This vehicle is insured, True or False?: {}'.format(bicycle.insured))
+print(bicycle.movement())
+'''
+
+class Person:
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def printName(self):
+        print('For this 2nd class, My name is {} {}\nstop'.format(self.firstName, self.lastName))
+
+myself = Person("Douglas", "Foreman")
+
+if __name__ == "__main__":
+    print(myself.printName())
