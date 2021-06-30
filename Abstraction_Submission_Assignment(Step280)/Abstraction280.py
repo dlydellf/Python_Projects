d6f94fb4dfe2ae5ce282b1dc19fc86a09e7e6918
DForeman <dlydellf@hotmail.com>
2021-06-29 23:15:29 -0500

    Created folder & beginning of Abstraction Assignment

---
+++ Abstraction_Submission_Assignment(Step280)/Abstraction280.py
@@ -0,0 +1,13 @@
+# Python Course - Step 280 ("Abstraction Submission Assignment")
+#   Created by: Douglas Foreman
+#   Created using: Python/IDLE v3.9.4
+#---------------------------------------
+#   Objectives of Step 280:
+#       a) Create a PARENT CLASS that uses abstraction,
+#       b) Parent class should contain one abstract method & one regular method,
+#       c) Create a CHILD CLASS that defines the implementation of its parent's abstract method,
+#       d) Create an OBJECT that utilizes both the parent & child methods.
+#---------------------------------------
+
+
+# The Parent class:
