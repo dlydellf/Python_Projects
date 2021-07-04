1e7b4ee1939feecb3d6f4dd72ce80f7369c460c5
DForeman <dlydellf@hotmail.com>
2021-07-04 15:47:24 -0500

    Created template for "Webpage Generator" Assignment

---
+++ Webpage_Generator_Submission_Assignment(Step_312)/Webpage_Generator_Pt1.py
@@ -0,0 +1,9 @@
+# Python Course - Step 310 ("Webpage Generator", Pt1)
+#   Created by: Douglas Foreman
+#   Created using: Python/IDLE v3.9.4
+#------------------------------------
+# Objective: Recreate a supplied GUI that allows users
+#            to select a directory and show that directory
+#            in a text field.
+#------------------------------------
+
