13374620975229d58c4c747aff0a7fb208db41d4
DForeman <dlydellf@hotmail.com>
2021-07-09 19:03:57 -0500

    Changed 'source' & 'destination' (within .py) after moving Folders A & B onto GitHub

--- File_Transfer_Submission_Assignment(Step_317)/File_Transfer.py
+++ File_Transfer_Submission_Assignment(Step_317)/File_Transfer.py
@@ -2,19 +2,33 @@
 #   Created by: Douglas Foreman
 #   Created using: Python/IDLE v3.9.4
 #------------------------------------
-# Objective: Create a script that will transfer
-#            files from one folder to another.
+# Objective: Part 1: Create a script that will transfer
+#                    files from one folder to another.
+#            Part 2: Create a script that 1) copies new files
+#                    (and those edited within 24hrs) to a
+#                    specific folder, and 2) transfers that
+#                    folder's contents to ANOTHER folder.
 #------------------------------------
+# Part 1:
 
 import shutil # imports the 'shutil' module
 import os
 
 # Sets the files' source path:
-source = '/Users/Michell/Desktop/FolderA/'
+source = './FolderA/'
 
 # Sets the destination path (to FolderB):
-destination = '/Users/Michell/Desktop/FolderB/'
+destination = './FolderB/'
 files = os.listdir(source) # Retrieves ALL files currently within the 'source' directory
-
 for i in files:
     shutil.move(source+i, destination) # Moves each file within the 'source' to its new destination
+'''
+#Part 2:
+import shutil
+import os
+
+s = '/Users/Michell/Desktop/FolderB/'
+print(os.lstat(s))
+
+#shutil.copy2(src, destination) # 'copy2' used here to attempt preserving files' metadata (creation/modification times, etc.)
+'''
