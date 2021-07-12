27993c914226eb1450dd6ea9a921cb793110c482
DForeman <dlydellf@hotmail.com>
2021-07-11 20:29:35 -0500

    Part 2 Complete - both functions are complete & gtg

--- File_Transfer_Submission_Assignment(Step_317)/File_Transfer.py
+++ File_Transfer_Submission_Assignment(Step_317)/File_Transfer.py
@@ -29,36 +29,31 @@ import shutil
 import os
 import datetime # importing the 'datetime' module instead of the 'time' module (thanks Hanna)
 
-here = './Part2/(start)New_and_Modified_Files_from_ALL_Users/' # the folder containing all of the day's .txt files...
+source = './Part2/New-Modified_Files_from_ALL_Users/' # the folder containing all of the day's .txt files...
+destination = './Part2/Destination_Folder(To_HomeOffice)/'
 
-# This () iterates through ALL files in above folder, capturing ONLY the .txt files:
-def captureTxtFiles():
-    allFiles = os.listdir(here) # '.listdir()' == the iterator
-    textFiles = [files for files in allFiles if ".txt" in files] # --> isolating ONLY the .txt files
+# This () iterates through ALL files in the source folder, creating an asolute path for ONLY the captured .txt files:
+def captureTxtFiles():    
     absoluteFilePaths = [] # An empty list to store each file's absolute file path
+    allFiles = os.listdir(source) # '.listdir()' == the iterator
+    textFiles = [files for files in allFiles if ".txt" in files] # --> isolating ONLY the .txt files
     for eachTxtFile in textFiles:
-        absoluteFilePath = os.path.join(here, eachTxtFile) # an absolute file path is REQUIRED to use 'os.path.getmtime' (below)!
+        absoluteFilePath = os.path.join(source, eachTxtFile) # an absolute file path is REQUIRED to use Line 52's 'os.path.getmtime'!!
         absoluteFilePaths.append(absoluteFilePath)
-    print(absoluteFilePaths)
+    lastModified(absoluteFilePaths) # Output becomes the next ()'s argument
 
-    
-    #lastModified(absoluteFilePath) # Output becomes the next ()'s argument
-'''
-# This () returns each .txt file's most recent modification time:
-def lastModified(absoluteFilePath):
-        modTime = os.path.getmtime(absoluteFilePath) # when was the file last modified (in epoch/seconds)?
-        localTime = datetime(modTime) # changes 'modTime' into the local time
-        hour = time.localtime(modTime).tm_hour # 'localtime(epoch).tm_hr' == returns the hour (in military time)
-        date = time.localtime(modTime).tm_mday
-        print(absoluteFilePath)
-        print(modTime)
-        
-# absoluteFilePath = (here + eachTxtFile) # Concatenates each .txt fileName & filePath into an absolute path
+# This () compares each .txt file's most recent modification time to the current time, copying those modified within the past 24hrs:
+def lastModified(absoluteFilePaths):
+    timeNow = datetime.datetime.now().timestamp() # current time, in seconds
+    time24hrsAgo = (timeNow-86400) # timeNow, minus a day's-worth of seconds (86,400)
+    #print('Current time is {}'.format(timeNow))
+    #print('  24hrs ago was {}'.format(time24hrsAgo))
+    for eachFile in absoluteFilePaths:
+        modTime = os.path.getmtime(eachFile) # when was each file last modified? (outputs in epoch/seconds)
+        #print("{}\nLast Mod Timestamp: {}\n HASH Timestamp(?): {}\n".format(eachFile, modTime, hash(modTime)))
+        if modTime >= time24hrsAgo:
+            shutil.copy2(eachFile, destination) # using 'copy2' instead of 'copy' to attempt preserving files' metadata (creation/modification times, etc).
+    print(os.listdir(destination))         
 
-'''
 if __name__ == "__main__":
     captureTxtFiles()
-
-
-#shutil.copy2(src, destination) # 'copy2' used here to attempt preserving files' metadata (creation/modification times, etc.)
-
